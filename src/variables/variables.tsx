c589db5ca1d4b14226776faaad58993ce3689dc1
jessicaagren <jessicaagren@hotmail.com>
2025-06-18 21:18:36 +0200

    Add variables

---
+++ src/variables/variables.tsx
@@ -0,0 +1,14 @@
+export const colors = {
+  orange: '#F2A74B',
+  white: '#F2F2F2',
+  black: '#333',
+  gray: '#D9D9D9',
+  darkgray: '#868686',
+  red: '#F20530',
+  green: '#63A621',
+};
+
+export const fonts = {
+  heading: 'Fredoka_600SemiBold',
+  body: 'System',
+};
